8a797a1a429de3746eb607ee8ca5fc3f9058caf0
Aidan Katz <aidankatz1835@gmail.com>
2023-04-08 23:27:24 -0400

    added main() and program logic

--- iracing-slip-tool.py
+++ iracing-slip-tool.py
@@ -1,12 +1,12 @@
 import pyirsdk as irsdk
 
-def calculate_slip_angle(telemetry):
+def calculate_slip_angle(data):
     # Get driver telemetry variables
-    velocity_x = telemetry['VelocityX']
-    velocity_y = telemetry['VelocityY']
-    yaw_rate = telemetry['YawRate']
-    steer_angle = telemetry['SteeringWheelAngle']
-    lateral_accel = telemetry['LateralAcceleration']
+    velocity_x = data['VelocityX']
+    velocity_y = data['VelocityY']
+    yaw_rate = data['YawRate']
+    steer_angle = data['SteeringWheelAngle']
+    lateral_accel = data['LateralAcceleration']
 
     # Calculate slip angle using the equation
     if velocity_x != 0:
@@ -15,3 +15,25 @@ def calculate_slip_angle(telemetry):
         slip_angle = 0
 
     return slip_angle
+
+def main():
+    iRacing = irsdk.IRSDK()
+    iRacing.startup()
+
+    while True:
+        if iRacing.is_initialized() and iRacing.is_connected():
+            # Check if car is on track
+            if iRacing['IsOnTrack'] == 1:
+                data = iRacing.get_session_data()
+                slip_angle = calculate_slip_angle(data)
+
+                # Display slip angle
+                print(f"Slip Angle: {slip_angle:.2f} degrees")
+
+        # Sleep to avoid overloading the CPU
+        time.sleep(0.05)
+
+    iRacing.shutdown()
+
+if __name__ == '__main__':
+    main()
